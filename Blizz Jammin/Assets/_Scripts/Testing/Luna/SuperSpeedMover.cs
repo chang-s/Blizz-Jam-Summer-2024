2f18b822d4e26d6ebe9327adcf64447f08676e1e
Albert Hou <alberthou2002@gmail.com>
2024-06-05 02:46:17 -0400

    Created simple inventory

--- Blizz Jammin/Assets/_Scripts/Testing/Luna/SuperSpeedMover.cs
+++ Blizz Jammin/Assets/_Scripts/Testing/Luna/SuperSpeedMover.cs
@@ -6,10 +6,12 @@ namespace _Scripts.Testing.Luna
     public class SuperSpeedMover : MonoBehaviour
     {
         [SerializeField] private float _baseMovementSpeed;
+        [SerializeField] private float _scale;
 
         private void FixedUpdate()
         {
-            transform.position += new Vector3(_baseMovementSpeed * Time.fixedDeltaTime, 0, 0);
+            transform.position += new Vector3(_baseMovementSpeed * Time.fixedDeltaTime,0 , 0);
+            transform.localScale += new Vector3(_scale * 2 * Time.fixedDeltaTime, _scale * Time.fixedDeltaTime);
         }
     }
 }
