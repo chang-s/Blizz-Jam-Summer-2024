c91a2eb68bf5bc4a16dcc87f4f11a8fe974198b2
ZotChryis <navac@outlook.com>
2024-06-20 22:10:46 -0700

    Monster prefab updates for long monster names
    And some monster data

--- Blizz Jammin/Assets/_Scripts/Gameplay/RecruitManager.cs
+++ Blizz Jammin/Assets/_Scripts/Gameplay/RecruitManager.cs
@@ -3,6 +3,7 @@ using Utility.Observable;
 
 namespace _Scripts.Gameplay
 {
+    // TODO: Should this just live in MonsterManager too? perhaps
     public class RecruitManager : MonoBehaviour
     {
         public Observable<int> Infamy { get; private set; } = new Observable<int>(0);
