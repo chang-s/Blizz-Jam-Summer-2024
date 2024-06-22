da51cf25e8065fde29a44a538a29da835bdc7904
ZotChryis <navac@outlook.com>
2024-06-22 13:36:39 -0700

    Fix prefab bugs

--- Blizz Jammin/Assets/_Scripts/Gameplay/StatModifier.cs
+++ Blizz Jammin/Assets/_Scripts/Gameplay/StatModifier.cs
@@ -13,23 +13,21 @@ namespace _Scripts.Gameplay
         
         /// <summary>
         /// The flat amount to delta on that stat.
-        /// TODO: Consider PercentAmount too, if we have time
         /// </summary>
         [BoxGroup("Modifier")]
-        public int Amount;
+        public int FlatAmount;
+        
+        /// <summary>
+        /// The flat amount to delta on that stat.
+        /// </summary>
+        [BoxGroup("Modifier")]
+        public float MultAmount;
         
         /// <summary>
         /// Whether or not the modifier is applied to the whole party.
         /// </summary>
         [BoxGroup("Modifier")]
         public bool Aura;
-        
-        /// <summary>
-        /// If supplied, all loot must be present in the wielder of this loot for this modifier to take effect.
-        /// "If wielder also has Scales equipped, gain +10 Endurance"
-        /// </summary>
-        [BoxGroup("Requirements")]
-        public SchemaLoot[] RequiredLoot;
 
         /// <summary>
         /// If supplied, all quirks must be present on the wielder for the modifier to take effect.
@@ -38,20 +36,28 @@ namespace _Scripts.Gameplay
         [BoxGroup("Requirements")]
         public SchemaQuirk[] RequiredQuirk;
         
-        // TODO: These two categories are stretch goals, if we have time
+        // TODO: Stretch goals
+        /*
         /// <summary>
         /// If supplied, all loot must be present in the party for this modifier to take effect.
         /// "If party has walkie-talkie, +10 Luck"
         /// </summary>
         [BoxGroup("Requirements")] 
         public SchemaLoot[] RequiredLootParty;
-        
-        // TODO: These two categories are stretch goals, if we have time
+
+        /// <summary>
+        /// If supplied, all loot must be present in the wielder of this loot for this modifier to take effect.
+        /// "If wielder also has Scales equipped, gain +10 Endurance"
+        /// </summary>
+        [BoxGroup("Requirements")]
+        public SchemaLoot[] RequiredLoot;
+
         /// <summary>
         /// If supplied, all loot must be present in the party for this modifier to take effect.
         /// "If party has a Sneaky and Slimy monster, +10 Luck"
         /// </summary>
         [BoxGroup("Requirements")] 
         public SchemaLoot[] RequiredQurikParty;
+        */
     }
 }
